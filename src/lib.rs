2fc4d231457a488bb054decb68b7a3c02a3fa16d
Jeff Muizelaar <jrmuizel@gmail.com>
2018-11-11 17:50:00 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,271 @@
+const BILINEAR_INTERPOLATION_BITS: u32 = 4;
+
+pub struct Bitmap {
+    width: i32,
+    height: i32,
+    data: Vec<u32>,
+}
+
+// we can reduce this to two multiplies
+// http://stereopsis.com/doubleblend.html
+// t is 0..256
+fn lerp(a: u32, b: u32, t: u32) -> u32
+{
+    let mask = 0xff00ff;
+    let brb = ((b & 0xff00ff) * t) >> 8;
+    let bag = ((b >> 8) & 0xff00ff) * t;
+    let t = 256-t;
+    let arb = ((a & 0xff00ff) * t) >> 8;
+    let aag = ((a >> 8) & 0xff00ff) * t;
+    let rb = arb + brb;
+    let ag = aag + bag;
+    return (rb & mask) | (ag & !mask);
+}
+
+
+struct GradientStop {
+    position: f32,
+    color: u32
+}
+
+pub struct GradientSource {
+    matrix: MatrixFixedPoint,
+    lut: [u32; 256],
+}
+impl GradientSource {
+    pub fn radial_gradient_eval(&self, x: u16, y: u16) -> u32 {
+        let p = self.matrix.transform(x, y);
+        let mut distance = (p.x as f32).hypot(p.y as f32) as u32;
+        distance >>= 8;
+        if distance > 32768 {
+            distance = 32786;
+        }
+        self.lut[(distance >> 7) as usize]
+    }
+
+    pub fn linear_gradient_eval(&self, x: u16, y: u16) -> u32 {
+        let p = self.matrix.transform(x, y);
+        let mut lx = p.x >> 16;
+        if lx > 256 {
+            lx = 265
+        }
+        if lx < 0 {
+            lx = 0;
+        }
+        self.lut[lx as usize]
+    }
+}
+
+pub struct Gradient {
+    stops: Vec<GradientStop>
+}
+impl Gradient {
+    pub fn make_source(&self, matrix: &MatrixFixedPoint) -> Box<GradientSource> {
+        let mut source = Box::new(GradientSource { matrix: (*matrix).clone(), lut: [0; 256]});
+        self.build_lut(&mut source.lut);
+        source
+    }
+    fn build_lut(&self, lut: &mut [u32; 256]) {
+
+        let mut stop_idx = 0;
+        let mut stop = &self.stops[stop_idx];
+
+        let mut last_color = stop.color;
+        let mut last_pos = 0;
+
+        let mut next_color = last_color;
+        let mut next_pos = (256. * stop.position) as u32;
+
+        let mut i = 0;
+        while i <= 256 {
+            while next_pos <= i {
+               stop_idx += 1;
+                last_color = next_color;
+                if stop_idx >= self.stops.len() {
+                    stop = &self.stops[stop_idx];
+                    next_pos = 256;
+                    next_color = stop.color;
+                    break;
+                }
+                next_pos = (256. * stop.position) as u32;
+                next_color = stop.color;
+            }
+            let inverse = (256 * 256)/(next_pos-last_pos);
+            let mut t = 0;
+            // XXX we could actually avoid doing any multiplications inside
+            // this loop by accumulating (next_color - last_color)*inverse
+            while i <= next_pos {
+                lut[i as usize] = lerp(last_color, next_color, t >> 8);
+                t += inverse;
+                i += 1;
+            }
+            last_pos = next_pos;
+        }
+    }
+}
+
+
+fn get_pixel(bitmap: &Bitmap, mut x: i32, mut y: i32) -> u32 {
+    if x < 0 {
+        x = 0;
+    }
+    if x > bitmap.width {
+        x = bitmap.width
+    }
+
+    if y < 0 {
+        y = 0;
+    }
+    if y > bitmap.height {
+        y = bitmap.height;
+    }
+
+    return bitmap.data[(y * bitmap.width + x) as usize];
+}
+
+/* Inspired by Filter_32_opaque from Skia */
+fn bilinear_interpolation(
+                          tl: u32,
+                          tr: u32,
+                          bl: u32,
+                          br: u32,
+                          mut distx: u32,
+                          mut disty: u32,
+                         ) -> u32 {
+    let distxy;
+    let distxiy;
+    let distixy;
+    let distixiy;
+    let mut lo;
+    let mut hi;
+
+    distx <<= 4 - BILINEAR_INTERPOLATION_BITS;
+    disty <<= 4 - BILINEAR_INTERPOLATION_BITS;
+
+    distxy = distx * disty;
+    distxiy = (distx << 4) - distxy; /* distx * (16 - disty) */
+    distixy = (disty << 4) - distxy; /* disty * (16 - distx) */
+    distixiy = 16 * 16 - (disty << 4) - (distx << 4) + distxy; /* (16 - distx) * (16 - disty) */
+
+    lo = (tl & 0xff00ff) * distixiy;
+    hi = ((tl >> 8) & 0xff00ff) * distixiy;
+
+    lo += (tr & 0xff00ff) * distxiy;
+    hi += ((tr >> 8) & 0xff00ff) * distxiy;
+
+    lo += (bl & 0xff00ff) * distixy;
+    hi += ((bl >> 8) & 0xff00ff) * distixy;
+
+    lo += (br & 0xff00ff) * distxy;
+    hi += ((br >> 8) & 0xff00ff) * distxy;
+
+    ((lo >> 8) & 0xff00ff) | (hi & !0xff00ff)
+}
+
+const FIXED_FRACTION_BITS: u32 = 16;
+pub const FIXED_ONE: i32 = 1 << FIXED_FRACTION_BITS;
+
+fn bilinear_weight(x: Fixed) -> u32 {
+    // discard the unneeded bits of precision
+    let reduced = x >> (FIXED_FRACTION_BITS - BILINEAR_INTERPOLATION_BITS);
+    // extract the remaining fraction
+    let fraction = reduced & ((1 << BILINEAR_INTERPOLATION_BITS) - 1);
+    fraction as u32
+}
+
+type Fixed = i32;
+
+fn fixed_to_int(x: Fixed) -> i32 {
+    x >> FIXED_FRACTION_BITS
+}
+
+// there are various tricks the can be used
+// to make this faster. Let's just do simplest
+// thing for now
+pub fn float_to_fixed(x: f32) -> Fixed {
+    (x * (1 << FIXED_FRACTION_BITS) as f32) as i32
+}
+
+pub fn fetch_bilinear(bitmap: &Bitmap, x: Fixed, y: Fixed) -> u32 {
+    let dist_x = bilinear_weight(x);
+    let dist_y = bilinear_weight(y);
+
+    let x1 = fixed_to_int(x);
+    let y1 = fixed_to_int(y);
+    let x2 = x1 + 1;
+    let y2 = y1 + 1;
+
+    let tl = get_pixel(bitmap, x1, y1);
+    let tr = get_pixel(bitmap, x2, y1);
+    let bl = get_pixel(bitmap, x1, y2);
+    let br = get_pixel(bitmap, x2, y2);
+
+    bilinear_interpolation(tl, tr, bl, br, dist_x, dist_y)
+}
+
+struct PointFixedPoint {
+    x: Fixed,
+    y: Fixed,
+}
+
+#[derive(Clone)]
+pub struct MatrixFixedPoint {
+    xx: Fixed,
+    xy: Fixed,
+    yx: Fixed,
+    yy: Fixed,
+    x0: Fixed,
+    y0: Fixed,
+}
+
+impl MatrixFixedPoint {
+    fn transform(&self, x: u16, y: u16) -> PointFixedPoint {
+        let x = x as i32;
+        let y = y as i32;
+        // when taking integer parameters we can use a regular mulitply instead of a fixed one
+        PointFixedPoint {
+x: x * self.xx + self.xy * y + self.x0,
+       y: y * self.yy + self.yx * x + self.y0,
+        }
+    }
+}
+
+fn packed_alpha(x: u32) -> u32 {
+    x >> 24
+}
+
+// this is an approximation of true 'over' that does a division by 256 instead
+// of 255. It is the same style of blending that Skia does.
+pub fn over(src: u32, dst: u32) -> u32 {
+    let a = packed_alpha(src);
+    let a = 256 - a;
+    let mask = 0xff00ff;
+    let rb = ((dst & 0xff00ff) * a) >> 8;
+    let ag = ((dst >> 8) & 0xff00ff) * a;
+    src + (rb & mask) | (ag & !mask)
+}
+
+fn alpha_to_alpha256(alpha: u32) -> u32 {
+    alpha + 1
+}
+
+fn alpha_mul_inv256(value: u32, alpha256: u32) -> u32 {
+    let prod = 0xFFFF - value * alpha256;
+    return (prod + (prod >> 8)) >> 8;
+}
+
+pub fn over_in(src: u32, dst: u32, alpha: u32) -> u32 {
+    let src_alpha = alpha_to_alpha256(alpha);
+    let dst_alpha = alpha_mul_inv256(packed_alpha(src), src_alpha);
+
+    let mask = 0xFF00FF;
+
+    let src_rb = (src & mask) * src_alpha;
+    let src_ag = ((src >> 8) & mask) * src_alpha;
+
+    let dst_rb = (dst & mask) * dst_alpha;
+    let dst_ag = ((dst >> 8) & mask) * dst_alpha;
+
+    // we sum src and dst before reducing to 8 bit to avoid accumulating rounding erros
+    return (((src_rb + dst_rb) >> 8) & mask) | ((src_ag + dst_ag) & !mask);
+}
